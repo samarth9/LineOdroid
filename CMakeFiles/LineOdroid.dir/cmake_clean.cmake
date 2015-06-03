2372ac1f74053e35244ec621944267f373f9cc31
agjayant <agjayant@iitk.ac.in>
2015-06-03 23:01:24 +0000

    Line code for odroid

---
+++ CMakeFiles/LineOdroid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/LineOdroid.dir/LineOdroid.o"
+  "LineOdroid.pdb"
+  "LineOdroid"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/LineOdroid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
